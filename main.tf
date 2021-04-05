a39129dbf676a521b006562e39a34cbc61b55270
Antonio Cuenco <antonio.cuenco@qantasloyalty.com>
2021-04-05 12:00:49 +1000

    Updated to 3 available AZs

--- main.tf
+++ main.tf
@@ -3,132 +3,216 @@
 ###############################################################################
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.name_prefix}-vpc"
-  }
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 
 ###############################################################################
 # AWS Internet Gateway
 ###############################################################################
 resource "aws_internet_gateway" "internet_gw" {
+  count = var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
   vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.name_prefix}-internet-gw"
-  }
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 
 ###############################################################################
-# AWS Subnets - Public
+# Public Routes
+###############################################################################
+# Public route table
+resource "aws_route_table" "public" {
+  count = length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(
+    {
+      "Name" = format("%s-${var.public_subnet_suffix}", var.name)
+    },
+    var.tags
+  )
+}
+
+# Public route to access internet
+resource "aws_route" "public_internet_gateway" {
+  count = var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gw[0].id
+}
+
+###############################################################################
+# Private Routes
+###############################################################################
+# Private route table
+resource "aws_route_table" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format(
+        "%s-${var.private_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags
+  )
+}
+
+###############################################################################
+# Public Subnets
 ###############################################################################
 # Subnets
-resource "aws_subnet" "public_subnets" {
-  count                   = length(var.availability_zones)
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets) > 0 && (false == var.one_nat_gateway_per_az || length(var.public_subnets) >= length(var.azs)) ? length(var.public_subnets) : 0
+
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = element(var.availability_zones, count.index)
-  cidr_block              = element(var.public_subnets_cidrs_per_availability_zone, count.index)
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.name_prefix}-public-net-${element(var.availability_zones, count.index)}"
-  }
+  cidr_block              = element(concat(var.public_subnets, [""]), count.index)
+  availability_zone       = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-${var.public_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags
+  )
+}
+
+###############################################################################
+# Private Subnets
+###############################################################################
+# Subnets
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  vpc_id               = aws_vpc.vpc.id
+  cidr_block           = var.private_subnets[count.index]
+  availability_zone    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-${var.private_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags
+  )
+}
+
+##############
+# NAT Gateway
+##############
+locals {
+  nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : length(var.private_subnets)
+  nat_gateway_ips = split(
+    ",",
+    var.reuse_nat_ips ? join(",", var.external_nat_ip_ids) : join(",", aws_eip.nat.*.id),
+  )
 }
 
 # Elastic IPs for NAT
-resource "aws_eip" "nat_eip" {
-  count = var.single_nat ? 1 : length(var.availability_zones)
-  vpc   = true
-  tags = {
-    Name = "${var.name_prefix}-nat-eip-${element(var.availability_zones, count.index)}"
-  }
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway && false == var.reuse_nat_ips ? local.nat_gateway_count : 0
+
+  vpc = true
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-%s",
+        var.name,
+        element(var.azs, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags
+  )
 }
 
 # NAT Gateways
 resource "aws_nat_gateway" "nat_gw" {
-  count         = var.single_nat ? 1 : length(var.availability_zones)
-  allocation_id = var.single_nat ? aws_eip.nat_eip.0.id : element(aws_eip.nat_eip.*.id, count.index)
-  subnet_id     = var.single_nat ? aws_subnet.public_subnets.0.id : element(aws_subnet.public_subnets.*.id, count.index)
+  count = var.enable_nat_gateway ? local.nat_gateway_count : 0
 
-  tags = {
-    Name = "${var.name_prefix}-nat-gw-${element(var.availability_zones, count.index)}"
-  }
+  allocation_id = element(
+    local.nat_gateway_ips,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+  subnet_id = element(
+    aws_subnet.public.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
 
-  depends_on = [
-    aws_internet_gateway.internet_gw
-  ]
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-%s",
+        var.name,
+        element(var.azs, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags
+  )
+
+  depends_on = [aws_internet_gateway.internet_gw]
 }
 
-# Public route table
-resource "aws_route_table" "public_subnets_route_table" {
-  count  = length(var.availability_zones)
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.name_prefix}-public-rt-${element(var.availability_zones, count.index)}"
-  }
-}
+# Private route to access internet via NAT
+resource "aws_route" "private_nat_gateway" {
+  count = var.enable_nat_gateway ? local.nat_gateway_count : 0
 
-# Public route to access internet
-resource "aws_route" "public_internet_route" {
-  count = length(var.availability_zones)
-  depends_on = [
-    aws_internet_gateway.internet_gw,
-    aws_route_table.public_subnets_route_table,
-  ]
-  route_table_id         = element(aws_route_table.public_subnets_route_table.*.id, count.index)
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.internet_gw.id
-}
+  nat_gateway_id         = element(aws_nat_gateway.nat_gw.*.id, count.index)
 
-# Association of Route Table to Subnets
-resource "aws_route_table_association" "public_internet_route_table_associations" {
-  count          = length(var.public_subnets_cidrs_per_availability_zone)
-  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
-  route_table_id = element(aws_route_table.public_subnets_route_table.*.id, count.index)
-}
-
-###############################################################################
-# AWS Subnets - Private
-###############################################################################
-# Subnets
-resource "aws_subnet" "private_subnets" {
-  count                   = length(var.availability_zones)
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = element(var.availability_zones, count.index)
-  cidr_block              = element(var.private_subnets_cidrs_per_availability_zone, count.index)
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "${var.name_prefix}-private-net-${element(var.availability_zones, count.index)}"
+  timeouts {
+    create = "5m"
   }
 }
 
-# Private route table
-resource "aws_route_table" "private_subnets_route_table" {
-  count  = length(var.availability_zones)
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.name_prefix}-private-rt-${element(var.availability_zones, count.index)}"
-  }
+##########################
+# Route table association
+##########################
+# Association of Route Table to Public Subnets
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
 }
 
-# Private route to access internet
-resource "aws_route" "private_internet_route" {
-  count = length(var.availability_zones)
-  depends_on = [
-    aws_internet_gateway.internet_gw,
-    aws_route_table.private_subnets_route_table,
-  ]
-  route_table_id         = element(aws_route_table.private_subnets_route_table.*.id, count.index)
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = var.single_nat ? aws_nat_gateway.nat_gw.0.id : element(aws_nat_gateway.nat_gw.*.id, count.index)
-}
+# Association of Route Table to Private Subnets
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
 
-# Association of Route Table to Subnets
-resource "aws_route_table_association" "private_internet_route_table_associations" {
-  count     = length(var.private_subnets_cidrs_per_availability_zone)
-  subnet_id = element(aws_subnet.private_subnets.*.id, count.index)
+  subnet_id = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(
-    aws_route_table.private_subnets_route_table.*.id,
-    count.index,
+    aws_route_table.private.*.id,
+    var.single_nat_gateway ? 0 : count.index,
   )
 }
--- variables.tf
+++ variables.tf
@@ -1,37 +1,100 @@
 ###############################################################################
-# Misc
-###############################################################################
-variable "name_prefix" {
-  description = "Name prefix for resources on AWS"
-}
-
-###############################################################################
 # AWS Virtual Private Network
 ###############################################################################
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
 variable "vpc_cidr_block" {
   description = "AWS VPC CIDR Block"
 }
 
-###############################################################################
-# AWS Subnets
-###############################################################################
-variable "availability_zones" {
-  type        = list(any)
-  description = "List of availability zones to be used by subnets"
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
 }
 
-variable "public_subnets_cidrs_per_availability_zone" {
+variable "public_subnets" {
   type        = list(any)
   description = "List of CIDRs to use on each availability zone for public subnets"
 }
 
-variable "private_subnets_cidrs_per_availability_zone" {
+variable "private_subnets" {
   type        = list(any)
   description = "List of CIDRs to use on each availability zone for private subnets"
 }
 
-variable "single_nat" {
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
   type        = bool
   default     = false
-  description = "enable single NAT Gateway"
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_igw" {
+  description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them."
+  type        = bool
+  default     = true
+}
+
+variable "public_subnet_suffix" {
+  description = "Suffix to append to public subnets name"
+  type        = string
+  default     = "public"
+}
+
+variable "private_subnet_suffix" {
+  description = "Suffix to append to private subnets name"
+  type        = string
+  default     = "private"
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Should be false if you do not want to auto-assign public IP on launch"
+  type        = bool
+  default     = true
+}
+
+variable "reuse_nat_ips" {
+  description = "Should be true if you don't want EIPs to be created for your NAT Gateways and will instead pass them in via the 'external_nat_ip_ids' variable"
+  type        = bool
+  default     = false
+}
+
+variable "external_nat_ip_ids" {
+  description = "List of EIP IDs to be assigned to the NAT Gateways (used in combination with reuse_nat_ips)"
+  type        = list(string)
+  default     = []
+}
+
+variable "one_nat_gateway_per_az" {
+  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
+  type        = bool
+  default     = false
+}
+
+variable "enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
+  default     = false
 }
